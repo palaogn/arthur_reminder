4075e83e5257cdbd0cc9d8b88b56488c332b2726
palaogn <palaogn@gmail.com>
2017-08-07 19:05:56 +0000

    added case for changing time to 12:00 and 09:00

--- app.js
+++ app.js
@@ -163,6 +163,8 @@ function processMessage(event) {
         case String(formattedMsg.match(/.*sos.*/)):
           sendMessage(senderId, {text: "Hey there I see you are in trouble. Ask me to reschedule if you want to reschedule your reminders and to delete your reminder if you want to delete them."});
           break;
+        case "12:00":
+        case "09:00":
         case String(formattedMsg.match(/.*^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$.*/)):
 			    updateDatabase(senderId, formattedMsg);
 			    triggerMessagejob(senderId, formattedMsg);
